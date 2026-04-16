718f63b95895d5cbbe1de57c6891740322cd45ad
ldamoredev <lauti.damore@gmail.com>
2026-04-16 11:59:26 -0300

    test: remove extra watchman setting

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: "jest-expo",
   testMatch: ["<rootDir>/src/**/*.test.ts", "<rootDir>/src/**/*.test.tsx"],
-  watchman: false,
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native|react-native|expo(nent)?|@expo(nent)?/.*|expo-.*|@expo/.*|@react-navigation/.*)/)",
   ],
